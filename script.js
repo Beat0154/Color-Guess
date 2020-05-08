91aa3191118f07b916dcbfc3db13bd567f83c8ad
Shawn Beaton <beat0154@algonquinlive.com>
2020-05-08 00:14:18 -0400

    files

---
+++ script.js
@@ -0,0 +1,63 @@
+var colors = ["Blue", "Red", "Green", "Yellow"];
+function startGame(){
+    setInterval(timer, 1000);
+    reset();
+}
+
+var correct = document.getElementById("correct");
+var correctInt = 0
+function checkColor(color, correctAnswer){
+    if(color==correctAnswer){
+        correctInt++;
+    }
+    reset();
+    correct.innerHTML = correctInt;
+}
+
+function reset(){
+    var random1 = Math.floor(Math.random()*2);
+    if(random1==0){
+        var color1 = document.getElementById("color1");
+        var color2 = document.getElementById("color2");
+    }else{
+        var color2 = document.getElementById("color1");
+        var color1 = document.getElementById("color2");
+    }
+    document.getElementById("start").style.display = "none";
+    var random = Math.floor(Math.random() * 4);
+    var correctAnswer = colors[random];
+    color1.innerHTML = correctAnswer;
+    color2.style.color = correctAnswer;
+    if(random+1==4){
+        color2.innerHTML = colors[random-3];
+    }else{
+        color2.innerHTML = colors[random+1];
+    }
+    if(random-1==-1){
+        color1.style.color = colors[random+3];
+    }else{
+        color1.style.color = colors[random-1];
+    }
+    color1.style.display = "block";
+    color2.style.display = "block";
+    addClick("Blue", correctAnswer);
+    addClick("Red", correctAnswer);
+    addClick("Green", correctAnswer);
+    addClick("Yellow", correctAnswer);
+}
+function addClick(color, correctAnswer){
+    var colorSpan = document.getElementById(color);
+    let onclick = "checkColor('".concat(color,"','",correctAnswer,"')");
+    colorSpan.setAttribute("onclick", onclick);
+}
+
+var countdown = 10;
+function timer(){
+    document.getElementById("time").innerHTML = countdown;
+    if(countdown==0){
+        clearInterval(timer);
+        alert("Game Over. Score: " + correctInt);
+        location.reload();
+    }
+    countdown--;
+}
